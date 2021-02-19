26a2f9e1e26d5d0ae37a507f971413971fd7df4b
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-19 18:15:23 +0000

    Create footer component, style tweaks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import Home from './components/home/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="app">
 		<Header />
 		<Home />
+		<Footer />
+
 		{/* Home */}
 			{/* Header */}
 
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.scss'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>© 2020 Airbnb Clone! No rights reserved - this is a demo! </p>
+            <p>Privacy &#183; Terms &#183; Sitemap &#183; Company Details</p>            
+        </div>
+    )
+}
+
+export default Footer
