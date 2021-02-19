a1952bd6603255f1d91b83e8e57060f21cb96b2d
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-19 14:53:31 +0000

    Setup header component
    Installed materual ui icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.scss';
+import Home from './components/home/Home'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="app">
-      <h1>React Airbnb</h1>
-      <h2>hello</h2>
-      <h3>htere</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error placeat illo quis quasi corporis recusandae repellat commodi laboriosam nemo porro.</p>
-      <a href="">a link</a>
-      <span>a span</span>
+		<Header />
+		<Home />
+		{/* Home */}
+			{/* Header */}
+
+			{/* Banner */}
+				{/* Search */}
+			
+			{/* Cards */}
+
+			{/* Footer */}
+		
+		{/* Search */}
+			{/* ... */}
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Header.scss'
+import SearchIcon from '@material-ui/icons/Search'
+import LanguageIcon from '@material-ui/icons/Language'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { Avatar } from '@material-ui/core'
+
+function Header() {
+    return (
+        <div className="header">
+            <img className="header__icon" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c513.png" alt="Airbnb Logo"/>
+
+            <div className="header__center">
+                <input className="header__input" type="text"/>
+                <SearchIcon />
+            </div>
+
+            <div className="header__right">
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
